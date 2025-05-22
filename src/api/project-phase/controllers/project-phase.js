8ac3d642b90a06879a0bf30383a403ee5fb0c9e8
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-05-22 09:43:04 +0300

    phase (#101)
    * phase
    
    * add phases block

--- src/api/project-phase/controllers/project-phase.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-phase controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::project-phase.project-phase');
