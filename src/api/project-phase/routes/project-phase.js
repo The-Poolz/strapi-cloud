e0985356c30cb2b64f0a64edd2c19d7ec104f8e3
Stan Goldin <salty87@gmail.com>
2025-05-22 09:37:31 +0300

    phase

--- src/api/project-phase/routes/project-phase.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-phase router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::project-phase.project-phase');
--- src/api/project-phase/services/project-phase.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-phase service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::project-phase.project-phase');
