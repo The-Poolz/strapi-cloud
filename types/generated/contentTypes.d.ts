ecbf08ad9bbf1d5545120fe7af86fb8633686b23
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-07-15 00:27:01 +0300

    Add is locked (#145)
    * feat: remove TheGraphUrl attribute from tokenomic schema and add IsLocked attribute to only-address component
    
    * feat: add MultiCallContractAddress attribute to tokenomic schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1983,9 +1983,10 @@ export interface ApiTokenomicTokenomic extends Struct.SingleTypeSchema {
       'api::tokenomic.tokenomic'
     > &
       Schema.Attribute.Private;
+    MultiCallContractAddress: Schema.Attribute.String &
+      Schema.Attribute.Required;
     PoolxToken: Schema.Attribute.String & Schema.Attribute.Required;
     publishedAt: Schema.Attribute.DateTime;
-    TheGraphUrl: Schema.Attribute.String & Schema.Attribute.Required;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
