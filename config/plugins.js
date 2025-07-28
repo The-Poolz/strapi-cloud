bb9b88ba6340a260a238257609a577033b0ef16c
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-07-28 19:10:51 +0300

    feat: add strapi-csv-import-export plugin to project dependencies and configuration (#154)

--- config/plugins.js
+++ config/plugins.js
@@ -8,4 +8,20 @@ module.exports = {
       },
     },
   },
+  "strapi-csv-import-export": {
+    config: {
+      authorizedExports: ["api::machine.machine",
+        "api::projects-information.projects-information",
+        "api::contracts-on-chain.contracts-on-chain",
+        "api::grow.grow",
+        "api::lock-token-whitelist.lock-token-whitelist",
+        "api::page-info.page-info"],
+      authorizedImports: ["api::machine.machine",
+        "api::projects-information.projects-information",
+        "api::contracts-on-chain.contracts-on-chain",
+        "api::grow.grow",
+        "api::lock-token-whitelist.lock-token-whitelist",
+        "api::page-info.page-info"],
+    }
+  },
 };
