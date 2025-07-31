826a06e95224626dda75f5cd6eb05a9c1f7c64ea
ArdenHide <ardenhide@gmail.com>
2025-07-31 15:50:56 +0300

    Remove `TheGraphURL` from `ContractOnChain`

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1001,7 +1001,6 @@ export interface ApiContractsOnChainContractsOnChain
     NativeCurrency: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     RPC: Schema.Attribute.String;
-    TheGraphURL: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
