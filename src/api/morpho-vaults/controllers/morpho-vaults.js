c49cbaf7513601f2f15b91077c6cc6c7e1ba0260
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-09-17 13:30:19 +0300

    add staking-vaults collection

---
+++ src/api/morpho-vaults/controllers/morpho-vaults.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * morpho-vaults controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::morpho-vaults.morpho-vaults');
---
+++ src/api/morpho-vaults/services/morpho-vaults.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * morpho-vaults service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::morpho-vaults.morpho-vaults');
